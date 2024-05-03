3879697a3e2bfff81cdd22f0b78592cb9b6e5e3c
Cody <codybarker.or@gmail.com>
2024-05-03 10:06:13 -0700

    correct palindrome regex

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,10 @@
 var isPalindrome = function (s) {
   return (
-    s.replace(/\W/g, "").toLowerCase().split("").reverse().join("") ===
-    s.replace(/\W/g, "").toLowerCase()
+    s.replace(/[\W_]/g, "").toLowerCase().split("").reverse().join("") ===
+    s.replace(/[\W_]/g, "").toLowerCase()
   );
 };
 
-const s = "racecars";
+const s = "A man, a plan, a canal: Panama";
 
 console.log(isPalindrome(s));
